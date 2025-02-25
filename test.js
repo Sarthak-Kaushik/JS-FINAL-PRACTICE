81ea5e9040f0a98bd8523a9bb08eff24ae7a4d83
Sarthak-Kaushik <Sarthak.kaushik99@gmail.com>
2025-02-25 14:11:39 +0530

    Starting JS Practice from the start

---
+++ test.js
@@ -0,0 +1,6 @@
+alert('Welcome Sarthak!');
+confirm('R U Sure Sarthak?');
+console.log('Hello Sarthak!!');
+document.write('Hello Sarthak kaushik!!');
+prompt('Enter your name Sarthak!!');
+// Output: Welcome Sarthak!
