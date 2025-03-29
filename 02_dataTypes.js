c81bc685d11d26be6760814a16445d07b5785b04
Sarthak Kaushik <Sarthak.kaushik2017@gmail.com>
2025-03-29 20:09:39 +0530

    dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,49 @@
+"use strict";
+// Primitive Data Types
+// 1. String
+let stuName = "John Doe";
+console.log(`Student's name is = ${stuName}`)
+
+//2. Number 
+let stuAge = 25;
+console.log(`Student's agi is = ${stuAge}`)
+
+//3. Boolean
+let isStu = true;
+console.log(`Is student = ${isStu}`)
+
+//4. Undefined
+let stuAddress;
+console.log(`Student's address is = ${stuAddress}`)
+
+//5. Null
+let stuGender = null;
+console.log(`Student's Gender is = ${stuGender}`);
+
+//6. BigInt
+let bigIntValue = BigInt(1234567890123456789012345678901234567890);
+console.log(`BigInt value is = ${bigIntValue}`);
+
+// 7. Symbol
+let sym1 = Symbol("sarthak");
+let sym2 = Symbol("sarthak");
+// Symbols are unique and immutable even if they have the same description
+// console.log(`Symbol value is = ${sym1}`);
+// console.log(`Symbol value is = ${sym2}`);
+// console.log(`Are both symbols equal? = ${sym1 === sym2}`); // false
+
+// Non-Primitive Data types
+
+// 1. Object
+
+//2. Array
+
+//3. Function
+
+// 4. Date
+
+// 5. RegExp
+
+// 6. Map
+
+// 7. Set
